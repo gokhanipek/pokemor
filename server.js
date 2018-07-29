47635f5aa465eb3f88a2380959abf0301b371197
gokhan <gipek03@gmail.com>
2018-07-29 17:13:55 +0300

    first deploy

--- server.js
+++ server.js
@@ -14,3 +14,19 @@ res.sendFile(path.join(__dirname+'/dist/<name-of-app>/index.html'));
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
+
+
+const forceSSL = function() {
+    return function (req, res, next) {
+      if (req.headers['x-forwarded-proto'] !== 'https') {
+        return res.redirect(
+         ['https://', req.get('Host'), req.url].join('')
+        );
+      }
+      next();
+    }
+  }
+
+
+
+  app.use(forceSSL());
